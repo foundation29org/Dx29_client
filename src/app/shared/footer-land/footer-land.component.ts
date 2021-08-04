ff16c053079c6e38d4a5c2033518cce0a1ffa00c
f29 javi <javier.logrono@outlook.com>
2021-08-04 17:37:07 +0200

    ADD TRACK info

--- src/app/shared/footer-land/footer-land.component.ts
+++ src/app/shared/footer-land/footer-land.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ToastrService } from 'ngx-toastr';
 
+declare let gtag: any;
+
 @Component({
     selector: 'app-footer-land',
     templateUrl: './footer-land.component.html',
@@ -11,11 +13,22 @@ import { ToastrService } from 'ngx-toastr';
 export class FooterLandComponent{
     //Variables
     currentDate : Date = new Date();
-
+    _startTime: any;
 
     constructor(public translate: TranslateService, public toastr: ToastrService) {
+        this._startTime = Date.now();
     }
 
+    lauchEvent(category) {
+        var secs = this.getElapsedSeconds();
+        gtag('event', sessionStorage.getItem('uuid'), { "event_category": category, "event_label": secs });
+      }
+    
+      getElapsedSeconds() {
+        var endDate = Date.now();
+        var seconds = (endDate - this._startTime) / 1000;
+        return seconds;
+      };
   
 
 }
